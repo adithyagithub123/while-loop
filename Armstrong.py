a87a38e02ddf9966b3a3bd0ea781e315faedda94
adithyagithub123 <novox1gamer@gmail.com>
2024-09-05 19:47:16 +0400

    while loop

---
+++ Armstrong.py
@@ -0,0 +1,16 @@
+n = int(input("Enter a number to check : "))
+sum = 0
+temp = n
+
+while temp>0 :
+    digit = temp % 10
+
+    sum += digit ** 3
+
+    temp //= 10
+
+if n == sum :
+    print(n , "is an armstrong number")
+else :
+    print(n , "is not an armstrong number")
+
